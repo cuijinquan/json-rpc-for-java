3840e1b32451a583ef26dd7d9e5b8a7628c13987
miracletalent <miracletalent@b39b0f77-0652-0410-a194-430ec2c6f52f>
2008-11-05 06:09:51 +0000

    X-UI中的相关Issues:YHXUIXM-37。

--- jcore/jsonrpc/common/JSONRPCBridge.java
+++ jcore/jsonrpc/common/JSONRPCBridge.java
@@ -42,6 +42,7 @@ public class JSONRPCBridge implements Serializable{
 		StringBuffer buf = new StringBuffer("{\"result\":[");
 		int n = 0;
 		Iterator oIt = topNms.entrySet().iterator();
+		System.out.println(topNms.size());
 		while(oIt.hasNext())
 		{
 			if(0 < n)
