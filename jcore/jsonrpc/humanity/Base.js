33a688f3dfa87c0cd0704c79ca67810a3b4a1b7c
jiangbo729 <jiangbo729@b39b0f77-0652-0410-a194-430ec2c6f52f>
2009-04-08 01:40:50 +0000

    改用Jquery的异步方式调用

--- jcore/jsonrpc/humanity/Base.js
+++ jcore/jsonrpc/humanity/Base.js
@@ -712,15 +712,30 @@ XuiLoading:function(o)
     /* data为请求刷新的对象，格式为[id,1或true表示过滤后面的字段,需要过滤的字段] */
     o.data.each(function(){s.push(this.join(","))});
     Base.AjaxObj = Ext.getBody();
+    ///*
+    $.ajax({
+    	cache:false,
+    	async:o.bAsync && !!o.fn,
+    	beforeSend:function(xml){
+        xml.setRequestHeader("XUIAJAX",1);
+			  xml.setRequestHeader("CMHS","JsonRpc");
+      },
+    	data: "__ajaxParam_=" + s.join('|') + s1.join("&"),
+    	url: o.url || document.location.href,
+    	type:"post",
+    	dataType:"html",
+    	complete:function(obj){o.fn(obj.responseText)}
+    	}); //*/
+    /* 
     JsonRpcClient().AJAX({
        data: "__ajaxParam_=" + s.join('|') + s1.join("&"),
        url: o.url || document.location.href,
        bAsync: o.bAsync && !!o.fn,
        clbkFun: function(){
        try {
-           o.fn && o.fn.apply(this, arguments);
+           o.fn && o.fn(arguments[0]);
        }catch(e){}
-       }});
+       }});*/
   }, /* 创建图层 */
   createDiv:function()
   {
