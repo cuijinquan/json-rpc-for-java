994b22c5abcff6186cd05f80f7fb688d6737d7c8
miracletalent <miracletalent@b39b0f77-0652-0410-a194-430ec2c6f52f>
2009-10-03 05:21:44 +0000

    增加不需要注册直接调用 jcore.jsonrpc.rpcobj包下的rpc服务类方法 ————强大的免注册功能

--- jcore/jsonrpc/servlet/JSONRPCServlet.java
+++ jcore/jsonrpc/servlet/JSONRPCServlet.java
@@ -2,6 +2,7 @@ package jcore.jsonrpc.servlet;
 
 import java.io.BufferedReader;
 import java.io.CharArrayWriter;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -26,140 +27,165 @@ import jcore.jsonrpc.tools.Tools;
  * @author 夏天
  */
 public class JSONRPCServlet extends HttpServlet {
-	private final static int buf_size = 4096;
+    private final static int buf_size = 4096;
 
-	private final static long serialVersionUID = 2;
+    private final static long serialVersionUID = 2;
 
-	private String charset = "UTF-8";
+    private String charset = "UTF-8";
 
-	// 支持Gzip，默认不支持
-//	private boolean bGzip = false;
-	// 安全检查的类
-	private ISecureCheck check = null;
-	private String secureCheck = null;
+    // 支持Gzip，默认不支持
+    // private boolean bGzip = false;
+    // 安全检查的类
+    private ISecureCheck check = null;
+    private String secureCheck = null;
 
-	private ServletConfig config = null;
+    private ServletConfig config = null;
 
-	public void destroy() {
-		super.destroy();
-		config = null;
-		check = null;
-		secureCheck = null;
-		this.charset = null;
+    public void destroy() {
+	super.destroy();
+	config = null;
+	check = null;
+	secureCheck = null;
+	this.charset = null;
+    }
+
+    public void init(ServletConfig config) throws ServletException {
+	this.config = config;
+	super.init(config);
+    }
+
+    public String rootPath = java.net.URLDecoder.decode(JSONRPCServlet.class.getResource("/").getFile());
+
+    public String szPkg = "/jcore/jsonrpc/rpcobj";
+    public boolean bInit = false;
+    // searchAllClass(JSONRPCServlet.class.getResource(szPkg).getFile());
+    public void searchAllClass(HttpServletRequest request, String s) {
+	if(bInit)return;
+	s = java.net.URLDecoder.decode(s);
+	File f = new File(s);
+	File[] fs = f.listFiles();
+	if(null == fs)return;
+//	System.out.println((null == fs) + "[" + fs.length + "]");
+	for (int i = 0; i < fs.length; i++) {
+	    if (fs[i].isDirectory())
+		searchAllClass(request, fs[i].getAbsolutePath());
+	    else {
+		String s1 = fs[i].getAbsolutePath().substring(rootPath.length() - 1);
+		s1 = s1.substring(0, s1.indexOf(".")).replaceAll("\\\\", ".");
+		String pknm = "jcore.jsonrpc.rpcobj";
+		if (s1.startsWith(pknm))
+		    try {
+			JsonRpcRegister.registerObject(request, s1.substring(pknm.length() + 1), Class.forName(s1));
+		    } catch (Exception e) {
+			e.printStackTrace();
+		    }
+	    }
 	}
+    }
 
-	public void init(ServletConfig config) throws ServletException {
-		this.config = config;
-		super.init(config);
+    /***************************************************************************
+     * 这里初始化启动的时候需要注册的对象
+     */
+    public void myInit(ServletConfig config, JSONRPCBridge brg) {
+	String szPam = null;
+	secureCheck = config.getInitParameter("secureCheck");
+	if (null != secureCheck) {
+	    try {
+		check = (ISecureCheck) Class.forName(secureCheck).newInstance();
+	    } catch (Exception e) {
+		e.printStackTrace();
+	    }
 	}
-
-	/*****************************************************************************
-	 * 这里初始化启动的时候需要注册的对象
-	 */
-	public void myInit(ServletConfig config, JSONRPCBridge brg) {
-		String szPam = null;
-		secureCheck = config.getInitParameter("secureCheck");
-		if(null != secureCheck)
-		{
-			try {
-				check = (ISecureCheck) Class.forName(secureCheck).newInstance();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+	charset = "UTF-8";
+	// 配置中指定默认支持gzip
+	// bGzip = "true".equals(config.getInitParameter("gzip"));
+	szPam = config.getInitParameter("regAppClassNames");
+	if (null != szPam && 0 < szPam.trim().length()) {
+	    szPam = szPam.replaceAll("[\\s]", "");
+	    String[] arrTmp = szPam.split("[;]");
+	    if (0 < arrTmp.length) {
+		for (int i = 0; i < arrTmp.length; i++) {
+		    String[] aT = arrTmp[i].split("[\\|:]");
+		    try {
+			Object o = Class.forName(aT[1]).newInstance();
+			brg.registerObject(aT[0], o);
+			o = null;
+		    } catch (Exception e) {
+			e.printStackTrace();
+		    }
+		    aT = null;
 		}
-		charset = "UTF-8";
-		// 配置中指定默认支持gzip
-//		bGzip = "true".equals(config.getInitParameter("gzip"));
-		szPam = config.getInitParameter("regAppClassNames");
-		if (null != szPam && 0 < szPam.trim().length()){
-			szPam = szPam.replaceAll("[\\s]", "");
-			String[] arrTmp = szPam.split("[;]");
-			if (0 < arrTmp.length){
-				for (int i = 0; i < arrTmp.length; i++){
-					String[] aT = arrTmp[i].split("[\\|:]");
-					try{
-						Object o = Class.forName(aT[1]).newInstance();
-						brg.registerObject(aT[0], o);
-						o = null;
-					}catch (Exception e){
-						e.printStackTrace();
-					}
-					aT = null;
-				}
-			}
-			arrTmp = null;
-		}
-		config = null;
+	    }
+	    arrTmp = null;
 	}
+	config = null;
+    }
 
-	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException,
-			ClassCastException {
-		// 安全处理
-		if(null != check && !check.secureCheck(request, response))
-			return;
-		HttpSession session = request.getSession(false);
-		
-		if (null == session)
-			session = request.getSession(true);
-		if (null != session){
-			JSONRPCBridge brg = (JSONRPCBridge) session.getAttribute(Content.RegSessionJSONRPCName);
-			// 如果是第一次就注册对象
-			if (null == brg){
-				session.setAttribute(Content.RegSessionJSONRPCName, brg = new JSONRPCBridge()
-						.setSession(session));
-			}
-			if(null != this.config)
-				myInit(this.config, brg);
-			JsonRpcRegister.registerObject(request, "_LoadJsObj", LoadJsObj.class);
-			OutputStream out = null;
-//			String szGzip = request.getHeader("Accept-Encoding");
-//			if (null != szGzip && -1 < szGzip.indexOf("gzip")
-//					&& (bGzip || "1".equals("JSONAccept-Encoding"))){
-//				response.setContentType("text/plain;charset=" + charset);
-//				response.setHeader("Content-Encoding", "GZIP");
-//				out = new GZIPOutputStream(response.getOutputStream());
-//			}
-//			else
-			{
-				response.setContentType("text/plain;charset=" + charset);
-				out = response.getOutputStream();
-			}
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException,
+	    ClassCastException {
+	// 安全处理
+	if (null != check && !check.secureCheck(request, response))
+	    return;
+	HttpSession session = request.getSession(false);
 
-			BufferedReader in = new BufferedReader(new InputStreamReader(request.getInputStream(),
-					charset));
+	if (null == session)
+	    session = request.getSession(true);
+	if (null != session) {
+	    JSONRPCBridge brg = (JSONRPCBridge) session.getAttribute(Content.RegSessionJSONRPCName);
+	    // 如果是第一次就注册对象
+	    if (null == brg) {
+		session.setAttribute(Content.RegSessionJSONRPCName, brg = new JSONRPCBridge().setSession(session));
+	    }
+	    if (null != this.config)
+		myInit(this.config, brg);
+	    JsonRpcRegister.registerObject(request, "_LoadJsObj", LoadJsObj.class);
+	    OutputStream out = null;
+	    // String szGzip = request.getHeader("Accept-Encoding");
+	    // if (null != szGzip && -1 < szGzip.indexOf("gzip")
+	    // && (bGzip || "1".equals("JSONAccept-Encoding"))){
+	    // response.setContentType("text/plain;charset=" + charset);
+	    // response.setHeader("Content-Encoding", "GZIP");
+	    // out = new GZIPOutputStream(response.getOutputStream());
+	    // }
+	    // else
+	    {
+		response.setContentType("text/plain;charset=" + charset);
+		out = response.getOutputStream();
+	    }
 
-			// 读取request中的JSON对象
-			CharArrayWriter data = new CharArrayWriter();
-			char buf[] = new char[buf_size];
-			int ret;
-			while ((ret = in.read(buf, 0, buf_size)) != -1)
-				data.write(buf, 0, ret);
+	    BufferedReader in = new BufferedReader(new InputStreamReader(request.getInputStream(), charset));
 
-			String szData = data.toString();
-			data.close();
-			data = null;
-			in.close();
-			in = null;
-			byte[] bout = null;
-			if (null != szData && 0 < szData.length())
-			{
-				Object obj = brg.ExecObjectMethod(request, szData);
-				if(null != obj)
-				   bout = Tools.encodeUnicodeHtm(obj.toString()).getBytes("UTF-8");
-			}
-			// 返回注册中的对象
-			else{
-				bout = brg.getRegObjsToString().getBytes();
-			}
-			if(null != bout)
-			{
-				response.setIntHeader("Content-Length", bout.length);
-				out.write(bout);
-			}
-			out.flush();
-			out.close();
-			session.setAttribute(Content.RegSessionJSONRPCName, brg);
-			
-		}
+	    // 读取request中的JSON对象
+	    CharArrayWriter data = new CharArrayWriter();
+	    char buf[] = new char[buf_size];
+	    int ret;
+	    while ((ret = in.read(buf, 0, buf_size)) != -1)
+		data.write(buf, 0, ret);
+
+	    String szData = data.toString();
+	    data.close();
+	    data = null;
+	    in.close();
+	    in = null;
+	    byte[] bout = null;
+	    if (null != szData && 0 < szData.length()) {
+		Object obj = brg.ExecObjectMethod(request, szData);
+		if (null != obj)
+		    bout = Tools.encodeUnicodeHtm(obj.toString()).getBytes("UTF-8");
+	    }
+	    // 返回注册中的对象
+	    else {
+		bout = brg.getRegObjsToString().getBytes();
+	    }
+	    if (null != bout) {
+		response.setIntHeader("Content-Length", bout.length);
+		out.write(bout);
+	    }
+	    out.flush();
+	    out.close();
+	    session.setAttribute(Content.RegSessionJSONRPCName, brg);
+	    searchAllClass(request, JSONRPCServlet.class.getResource(szPkg).getFile());
+	    bInit = true;
 	}
+    }
 }
