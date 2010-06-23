5564b31fdeb20d9cd5801386bf1dcede559e663e
lsj3077 <lsj3077@gmail.com>
2010-06-23 07:12:14 +0000

    新增多选下拉

--- jcore/jsonrpc/humanity/MultiSelect.js
+++ jcore/jsonrpc/humanity/MultiSelect.js
@@ -1,32 +1,56 @@
 {
-	/*失去焦点触发事件，使swf隐藏*/
-	myBlur:function (id,e){
-	  var o = getMultiSwf("__multiSelectSwf_" + id);
-	  o && o.closeMultiSelect && o.closeMultiSelect();
-	},
-	/**隐藏swf*/
-	hideSWF:function (o){
-	  Base.setValue(o.p, o.sv);
-	  Base.setValue(("_mask_multi" + o.p), o.v);
-	  var div = $("#__multiComboBoxDiv_" + o.id);
-	  div && div.css({left:"-1000px", top: "-1000px"});//避免swf的回调函数，在div隐藏后失效
-	},
-	/*显示swf的多选下拉框*/
-	openSwf:function (input){
-	  input && input.blur();//使input失去焦点，以免在swf上和input产生2个光标 
-	  var multiid = $(input).attr("multiid"), div = $("#__multiComboBoxDiv_" + multiid),
-	      oPos = $(input).position(),//定位swf的div的位置,输入框相对父容器的位置
-	      o = getMultiSwf("__multiSelectSwf_" + multiid);
-	  //o.width = $(input).width() + 2; //1.OBJECT宽度的控制 : 1.IE第一次不能弹出下拉框 2.FIXME:firefox not work    
-	  div.css({left:oPos.left + "px", top: (oPos.top) + "px"});
-	  if (o){
-	    o.focus();//触发onfocus事件: 1.好处是，打开swf的div时，就将焦点置于swf上，以便在焦点失去的时候，触发object的onblur事件
-	              //                2.坏处是，第一次点击的时候，会使swf消失,
-	    o.openMultiSelect && o.openMultiSelect();      
+	  openMultiSelectDiv:function (e,o1,input){
+	  var o = $("#"+o1.id);
+	  input = $(input);
+	  o.attr("class","MultiSelectShow");
+	  oPos=input.position();    
+	  o.css({left:oPos.left+"px",top: (oPos.top+input.height()+4)+"px",width:(o1 && o1.width && parseInt(o1.width) || (input.width()+15))+"px"});
+	
+	  o.find("Object,embed").width(o.width() + "px");
+	  var _o=o.find("#buttons");_o.css({"background-position":"1px","margin-top":"10px","margin-right":(o.width()-_o.width())/2});
+	  _o.find(".z-btn").css({"margin-right":"10px"});
+	  //处理文本框过滤
+	  var colsArr = eval($(o.find(".x-hide-display")[0]).text()),s="";
+	  for(i = 0;i<colsArr.length;i++){
+	  	s+=colsArr[i]["name"]+",";
 	  }
-	},
-	/*根据swf的id获取OBJECT对象*/
-	getMultiSwf:function (id){
-	    return -1 != navigator.appName.indexOf("Microsoft") ? window[id] || document.getElementById(id): document[id];
-	}
+	  var dg=o.find("Object,embed"),_dg_id=dg.attr("id");
+	  _dg_id=_dg_id.substr(0,_dg_id.length-1);
+	  input.keyup(function(){
+	  	_dg_id.setFilterExpression(this,s);
+	  });
+	  //一系列事件
+	  o.keypress(function(e){
+	  	if(e.keyCode==27)MultiSelect.hiddenMultiSelectDiv(o1.id);
+	  });
+	  input.keypress(function(e){
+	  	if(e.keyCode==27)MultiSelect.hiddenMultiSelectDiv(o1.id);
+	  });
+	  $("#MultiSelect-Close-Butt").click(function(){
+	  	MultiSelect.hiddenMultiSelectDiv(o1.id);
+	  });
+	  $("#MultiSelect-OK-Butt").click(function(){
+	  	MultiSelect.selectedOk(o,input,_dg_id);
+	  });
+	  },
+	  hiddenMultiSelectDiv:function(id)
+	  {  
+	   	$("#"+id).attr("class","MultiSelectHid");
+	   	$("#"+id).css({left:"-3000px"});
+	  },
+	  selectedOk:function(o,input,_dg_id){
+	  	var oArr = $(_dg_id.fnGetChckAllRows()),s="[",displayFields = o.attr("displayFields");
+	  	if(oArr.size()!=0){ 
+		if(displayFields){
+		  	oArr.each(function(){
+		  		_t = this;
+				s+=_t[displayFields]+",";	
+		  	});
+		  	input.val("共"+oArr.size()+"项："+s+"]");}
+	  	else 
+	  		input.val("[已经选择 "+oArr.size()+" 项]")
+	  	}
+	  	else input.val("");
+	  	MultiSelect.hiddenMultiSelectDiv(o.attr("id"));
+	  }
 }
